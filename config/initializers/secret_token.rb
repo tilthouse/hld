4ddcddfc7be8e5b913566e4f767e9cd2301cbd7a
Alex Boster <alex@foodydirect.com>
2014-02-17 19:42:59 -0800

    Initial commit of empty default Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hld::Application.config.secret_key_base = 'eeaf05e3913eae0ec0166bede8eb9917b8072ab43021a3db36e3e04b9fd496b332b0937868e14b17a8d10787a7f80497ff07fec70cdd3364a58ced6e944fce16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hld::Application.config.session_store :cookie_store, key: '_hld_session'
